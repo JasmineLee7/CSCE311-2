69e667ed57626709b6403783ce5a4de15c9af6ea
JasmineLee7 <jgl4@email.sc.edu>
2026-02-05 10:22:46 -0500

    update

--- src/main.cc
+++ src/main.cc
@@ -1,55 +1,53 @@
 #include <iostream>
 #include "../lib/cli_parser.h"
 #include "../lib/error.h"
-//#include <proj1/lib/cli_proj1lib.a>
 #include "../lib/sha256.h"
 #include "../lib/thread_log.h"
 #include "../lib/timings.h"
 
-#include <iostream>
 #include <vector>
 #include <fstream>
 #include <sys/sysinfo.h>
 
-// validate that k<=n;
+// validate that k<=n, we need to do this atsp 
 int main(){
     // read file in 
    int n = get_nprocs();
    std::cout<<"nprocs: "<<n<<std::endl;
 
-
-   //struct Row;
-  // std::vector<Row> rows_in;
-
+   struct Row;
+   std::vector<Row> rows_in;
 
   // change back tty 
-  //std::ifstream tty_in("/dev/tty");
-  //    if (tty_in) {
-    //      int var;
-    //  tty_in >> var;
+  std::ifstream tty_in("/dev/tty");
+      if (tty_in) {
+          int var;
+      tty_in >> var;
+     }
+    int k = 1;
 
-    // prompt for "x"
+    // ask user for k
+    std::cout<<"Enter a value for K"<<std::endl;
+    std::cin>>k;
+    std::cout<<"You entered "<<k<<std::endl;
 
 
+    // make realse threads as described 
+    //-- all --rate --thread 
 
-   // int curr_thread = 1;
-   // int max_thread = n;
+    // keep curr_thread and max_thread='k'
+    int curr_thread = 0;
+    int max_thread = k;
 }
 
-// VS code really wants me to have a comment here
+// work loop, pay attention to time. it should time out after 
+//   CliMode mode;
+//   Time_T timeout_ms;
+//   CliParse(argc, argv, &mode, &timeout_ms);
 
-// read file in 
+void* StartRoutine(void* arg) {
+  int *index = static_cast<int *>(arg);
+  std::cout << "StartRoutine received index " << *index << std::endl;
 
-//struct Row;
-//std::vector<Row> rows_in;
-
-// change back tty 
-
-// prompt for "x"
-
-
-
-// make realse threats as described 
-//-- all --rate --thread 
-
-// keep curr_thread and max_thread='k'
+  return nullptr;
+}
