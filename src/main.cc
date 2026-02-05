6282d4b315714a50ab72ffb8c8e2d2fb3ce4c341
JasmineLee7 <jgl4@email.sc.edu>
2026-02-05 12:40:20 -0500

    stuff

--- src/main.cc
+++ src/main.cc
@@ -5,17 +5,24 @@
 #include <proj1/lib/thread_log.h>
 #include <proj1/lib/timings.h>
 
+#include <pthread.h>
 #include <vector>
 #include <fstream>
 #include <sys/sysinfo.h>
 
-void* StartRoutine(void* arg);
-
 struct ThreadDatum {
     int thread_index;
+    const int *k;
+    const *std::vector<Row> rows_in;
 };
 
-int main(){
+void* StartRoutine(void* arg);
+
+int main(int argc, char* argv[]) {
+    CliMode mode;
+    time_t timeout_ms;
+    liParse(argc, argv, &mode, &timeout_ms);
+
     // read file in 
    int n = get_nprocs();
    std::cout<<"nprocs: "<<n<<std::endl;
@@ -23,6 +30,18 @@ int main(){
     // ask user for k
     // change back tty 
     int k;
+    int curr_thread = 0;
+    int max_thread = k;
+
+    // "make realse threads as described" ---- Is this how you described it Lewis?
+    std::vector<ThreadDatum> thread_data;
+    for(int i=0; i< n-k; i++){
+        ThreadDatum td{i, &k, &rows_in};
+        pthread_create(&td.thread_index, NULL, &StartRoutine, &td);
+        td.thread_index = i+1; // thread index starts from 1
+        thread_data.push_back(td);
+    }
+
     std::ifstream tty_in("/dev/tty");
       if (tty_in) {
           int var;
@@ -31,24 +50,30 @@ int main(){
         std::cout<<"you entered Var: "<<var<<std::endl;
     }
 
-    // "make realse threads as described" ---- Is this how you described it Lewis?
-    std::vector<ThreadDatum> thread_data;
-    for(int i=0; i< n-k; i++){
-        ThreadDatum td;
-        td.thread_index = i+1; // thread index starts from 1
-        thread_data.push_back(td);
-    }
+     
+
+
+
+
+
+
+
 
     //-- all --rate --thread 
     // read in from file
     //struct Row;
     //std::vector<Row> rows_in;
-
-    int curr_thread = 0;
-    int max_thread = k;
 }
 
 // work loop, pay attention to time. it should time out after 
-//   CliMode mode;
-//   Time_T timeout_ms;
-//   CliParse(argc, argv, &mode, &timeout_ms);
+void* StartRoutine(void* arg) {
+
+    while(curr_thread < max_thread){
+        Timings_SleepMs(1000);
+    }
+
+  ThreadDatum *index = reinterpret_cast<ThreadDatum *>(arg);
+  std::cout << "StartRoutine received index " << *index->thread_index << std::endl;
+
+  return nullptr;
+}
